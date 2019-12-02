1e237e35d0b3a7e1654342f2d4d18fff034837f5
Jason <dimensions3@outlook.com>
2019-12-01 17:09:32 -0800

    wip

--- main.py
+++ main.py
@@ -11,35 +11,95 @@ GUI = Builder.load_file("main.kv")
 
 
 class TabLayout(TabbedPanel):
-    
+
+
     pass
 
 class AppButton(Button):
 
     def on_press(self):
-        if self.name is "DEPLOY_APP":
+        if self.name is "DEPLOY_APP_BUTTON":
             arweave_com.deploy_app()
+
+        elif self.name is "TEST_APP_BUTTON":
+            print("test app")
+
+        elif self.name is "CONTENT_PATH_LOAD_BUTTON":
+            print("load content path")
+
+        elif self.name is "THEME_LOAD_BUTTON":
+            print("load theme")
+
+        elif self.name is "GENERATE_KEY_BUTTON":
+            print("generate signature keys")
+        
+        elif self.name is "LOAD_KEY_BUTTON":
+            print("load signature keys")
+
+        elif self.name is "LOAD_WALLET_KEY_BUTTON":
+            print("load wallet key")
+
+        elif self.name is "CREATE_WALLET_KEY_BUTTON":
+            print("create wallet key")
+
+        elif self.name is "FORGET_WALLET_KEY_BUTTON":
+            print("forget wallet key")
+
         pass
 
 class AppTextInput(TextInput):
-    print(TextInput)
+    '''
+    Handle text inputs
+    '''
+    def on_text_input_change(self):
+        if self.name is "TITLE_TEXT_INPUT":
+            app_builder.ARWEAVE_APP_TITLE = self.text
 
-    def on_text_validate(self):
-        print(self.text)
+        elif self.name is "AUTHOR_TEXT_INPUT":
+            app_builder.AUTHOR = self.text
+            
+        elif self.name is "REFERNCES_TEXT_INPUT":
+            app_builder.REFERENCES = self.text
+
+class ConsoleTextInput(TextInput):
+    '''
+    Handle console text inputs
+    '''
+    def on_text_input_change(self):
+        if self.name is "TITLE_TEXT_INPUT":
+            app_builder.ARWEAVE_APP_TITLE = self.text
 
 class AppSpinner(Spinner):
-    print(Spinner)
+    '''
+    Handle the dropdowns
+    '''
+    def on_spinner_change(self):
+        print("on spinner change")
 
-    def on_release(self):
-        print(self.text)
+        if self.name is "TYPE_SPINNER":
+            app_builder.ARWEAVE_APP_TYPE = self.text
 
-
+        elif self.name is "CONTENT_TYPE_SPINNER":
+            app_builder.CONTENT_TYPE = self.text
         
+        elif self.name is "THEME_SPINNER":
+            app_builder.THEME = self.text
+
+        elif self.name is "SIGNATURE_SPINNER":
+            if self.text is "ON":
+                app_builder.SIGN_APP = True
+            else:
+                app_builder.SIGN_APP = False
+
+    
+
 
 
 
 class TabbedPanelApp(App):
     def build(self):
+        self.title = 'PUBLIC8'
+
         return TabLayout()
 
 
